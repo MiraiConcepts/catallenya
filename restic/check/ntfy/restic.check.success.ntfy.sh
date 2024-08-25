79089c8f8678beb409c68e2e4e2c868e5d2b6576
carrein <carreinlee@proton.me>
2024-08-25 01:55:34 -0700

    Configuration changes.

---
+++ restic/check/ntfy/restic.check.success.ntfy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+service_status=$(systemctl status restic.check.service)
+
+curl -H "Tags: green_heart" \
+     -H "Title: Restic Check Success" \
+     -d "$service_status" \
+     "https://catallenya-sev.REDACTED.ts.net:3000/restic"
